4b43083b7397fc849b3794de3ce74d9b2d371f2d
Nishkb <66142773+Nishkb@users.noreply.github.com>
2023-01-17 17:19:53 -0500

    Add files via upload

---
+++ basics/basics/basicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicsApp.swift
+//  basics
+//
+//  Created by Nishk Balsara on 2023-01-11.
+//
+
+import SwiftUI
+
+@main
+struct basicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
